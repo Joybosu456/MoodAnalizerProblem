162bc31dc7d9b22f136ac61b7f6591a097519f5d
JOY <joybosu@gmail.com>
2022-03-10 20:10:13 +0530

    UC1 and TC1 Program file pushing

---
+++ MoodAnalyzeDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerProblem
+{ 
+    public class MoodAnalyse
+    {
+        public string AnalyseMood(string message)
+        {
+            if (message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
